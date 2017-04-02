8a560822d276890dfd041ea248651cc4112af4c7
Matias <mpierdominici@itba.edu.ar>
2017-04-02 18:35:10 -0300

    w

---
+++ TP4-EDA-WORMS/TP4-EDA-WORMS/TP4-EDA-WORMS.cpp
@@ -0,0 +1,49 @@
+// TP4-EDA-WORMS.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "graficar.h"
+
+int main()
+{
+	/*
+	
+	int fps = 0;
+	float x = 10;
+	graficar GraficadorDeWorms;
+	if (GraficadorDeWorms.IsInitOk() == true)
+	{
+		for (int i = 10; i <400; i++)
+		{
+			GraficadorDeWorms.DrawBackgarund();
+			if (fps < 50)
+			{
+				if (fps < 8)
+
+				{
+					GraficadorDeWorms.SetWorm(x, 0, fps, 1, 1);
+
+				}
+				else
+				{
+					x += 0.6;
+					GraficadorDeWorms.SetWorm(x, 0, fps, 1, 1);
+				}
+			}
+			else
+			{
+				fps = 0;
+			}
+			
+			fps++;
+			
+			al_rest(1/50);
+		}
+		
+		GraficadorDeWorms.DestroyAll();
+	}
+	*/
+
+    return 0;
+}
+
